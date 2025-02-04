94a0f08bc9330382b76117f5cd56dc3a3e596a60
--unset <bpriyan18082004@gmail.com>
2025-02-04 22:45:33 +0530

    tailwind config added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{html,js,ts,jsx,tsx}",
+  ],
+    theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
